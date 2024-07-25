2c702c30c020f10296e50cca91b5062fb02a4d33
Kelv <k2ying@uwaterloo.ca>
2024-07-25 00:00:49 -0400

    Gotten a new dataset, made changes to be able to display bounding boxes and image

--- cust_dataset.py
+++ cust_dataset.py
@@ -9,27 +9,33 @@ import numpy as np
 
 class CelebDataset(Dataset): 
     
-    def __init__ (self, box_path, attr_path, landmarks_path,part_path,root_dir) -> None:
+    def __init__ (self, box_path, root_dir) -> None:
         
         self.to_tensor = transforms.ToTensor()
         self.data_info = pd.read_csv(box_path)
         self.root_dir = root_dir
         self.image_arr = np.asarray(self.data_info.iloc[:, 0])
-        self.attr_info = pd.read_csv(attr_path)
-        self.land_info = pd.read_csv(landmarks_path)
-        self.part_info = pd.read_csv(part_path)
+        
         ### need to store info like bounding boxes for different stuff
         self.data_len = self.data_info.shape[0]
         
     def __getitem__(self, index) -> tuple :
-        img_name = self.image_arr[index]
-        img_as_img = Image.open((self.root_dir + img_name))
-        #img_as_tensor = self.to_tensor(img_as_img)
-        x_1: int = self.data_info.iloc[index]['x_1']
-        y_1: int = self.data_info.iloc[index]['y_1']
-        box = [x_1, y_1, x_1 + self.data_info.iloc[index]['width'], y_1 + self.data_info.iloc[index]['height']]
         
-        return (box)
+        img_name = self.image_arr[index]
+        
+        
+        img_as_img = np.array(Image.open((self.root_dir + img_name)))
+        print(type(img_as_img))
+        img_as_tensor = self.to_tensor(img_as_img)
+        
+        #store bounding box information
+        x_0: int = self.data_info.iloc[index]['x0']
+        y_0: int = self.data_info.iloc[index]['y0']
+        x_1: int = self.data_info.iloc[index]['x1']
+        y_1: int = self.data_info.iloc[index]['y1']
+        box = (x_0, y_0, x_1, y_1)
+        
+        return (box , img_as_tensor)
 
     def __len__(self) -> int:
         return self.data_len
--- train.py
+++ train.py
@@ -1,21 +1,33 @@
 from cust_dataset import CelebDataset
+from torchvision.utils import draw_bounding_boxes
+import matplotlib.pyplot as plt
+import torch
+from torchvision import transforms
+
 
 def main() -> None:
-    test = CelebDataset('archive/list_bbox_celeba.csv',
-                        'archive/list_attr_celeba.csv',
-                        'archive/list_landmarks_align_celeba.csv',
-                        'archive/list_eval_partition.csv',
-                        './archive/img_align_celeba/img_align_celeba/')
-
+    test = CelebDataset('archive/faces.csv',
+                        './archive/images/')
+    #dat = pd.read_csv
     # for i, (boxes) in enumerate(test):
     #     x, y, xx, yy = boxes
     #     print(x,y,xx,yy)
         
         
-    (boxes) = next(iter(test))
+    boxes, img = next(iter(test))
     x, y, xx, yy = boxes
-    print('here')
     print(x,y,xx,yy)
+    
+    boxes_tensor = torch.tensor(boxes).unsqueeze(0)
+    img_bbox= draw_bounding_boxes(img , boxes_tensor, width = 3, colors = 'red')
+    
+    transform = transforms.Compose([
+        transforms.ToPILImage()
+    ])
+    plt.imshow(transform(img_bbox))
+    plt.show()
+    print('here')
+    #print(x,y,xx,yy)
     
 
 
